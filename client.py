d0ef844bc32f894dce9d5a40d660e1921b64f053
Kaczubie <zubiel.kacper@gmail.com>
2018-11-04 22:52:23 +0100

    add volume control and comments

--- client.py
+++ client.py
@@ -1,37 +1,42 @@
 import socket
 import pyaudio
+import struct
 
 
 
-def callback(input_data, frame_count, time_info, flags):
+def callback(input_data, frame_count, time_info, flags):  # audio sent only when available, silence ignored thanks to using callback
     frames.append(input_data)
     s.sendall(input_data)
     return input_data, pyaudio.paContinue
 
 
 #record
-CHUNK = 4096
+CHUNK = 4096   # big value here like 4096 may reduce lattency problem. in slow networks audio compression  may be needed -> may be added in next commits
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
-RATE = 48000
-RECORD_SECONDS = 40
+RATE = 48000    # RATE  and CHANNEL are device-dependent -> may be taken from device_info in next commits
+RECORD_SECONDS = 40    
 
-HOST = '192.168.8.104'    # The remote host
-PORT = 50007              # The same port as used by the server
+HOST = '192.168.0.22'    # The remote host
+PORT = 50007              # Port for audio data
 device_info = {}
+PORT2 = 50008              #Port for volume control. Both PORT and PORT2 should be set same on client and server
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
+v = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+v.connect((HOST, PORT2))
+
 
 p = pyaudio.PyAudio()
-device_info = p.get_device_info_by_index(4)
+device_info = p.get_device_info_by_index(4)  # in my case device_id = 4 is WASAPI output
 stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
                 input=True,
                 frames_per_buffer=CHUNK,
                 input_device_index = device_info["index"],
-                stream_callback=callback,
+                stream_callback=callback,  #gets soundcard output sound without using any microphone
                 as_loopback=True)
 
 print("*recording")
@@ -39,7 +44,9 @@ print("*recording")
 frames = []
 
 while(1):
-    pass
+
+    vol_input = input("change volume?")
+    v.sendall(bytes(str(vol_input), 'utf8'))  #whenever hit enter new volume value send to UNIX device runnig server and being set. If server is running on Windows device it wont work
 
 print("*done recording")
 
--- server.py
+++ server.py
@@ -1,41 +1,70 @@
-# Echo server program
 import socket
 import pyaudio
 import wave
-import time
+import subprocess
 
-CHUNK = 4096
-FORMAT = pyaudio.paInt16
-CHANNELS = 2
-RATE = 48000
+
+def set_master_volume(volume):
+    val = float(int(volume))
+    proc = subprocess.Popen('/usr/bin/amixer sset Master ' + str(val) + '%', shell=True, stdout=subprocess.PIPE)  #set volume on unix device
+    proc.wait()
+
+
+CHUNK = 4096  #same value as set on client 
+FORMAT = pyaudio.paInt16 #same value as set on client 
+CHANNELS = 2  #same value as set on client 
+RATE = 48000  #same value as set on client 
 RECORD_SECONDS = 40
 WAVE_OUTPUT_FILENAME = "server_output.wav"
 WIDTH = 2
 frames = []
 
 p = pyaudio.PyAudio()
+
 stream = p.open(format=p.get_format_from_width(WIDTH),
                 channels=CHANNELS,
                 rate=RATE,
                 output=True,
                 frames_per_buffer=CHUNK)
 
+HOST = ''  # Symbolic name meaning all available interfaces
+PORT = 50007  # Port for audio data  Arbitrary non-privileged port
+PORT2 = 50008  #Port for volume control. Both PORT and PORT2 should be set same on client and server
 
-HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 50007              # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+v = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
+v.bind((HOST, PORT2))
+
 s.listen(1)
-conn, addr = s.accept()print 'Connected by', addr
+v.listen(1)
+conn, addr = s.accept()
+print('Connected by', addr)
+conn2, addr2 = v.accept()
+conn2.setblocking(0)  #doesnt block when volume info is not available
+print('super dziala', addr2)
 data = conn.recv(4096)
 
-i=1
+i = 1
+
 while data != '':
+
     stream.write(data)
     data = conn.recv(4096)
-    i=i+1
-    print i
+    i = i + 1
+    print(i)
     frames.append(data)
+    if i == 40:  #checking for volume data less often than getting audio data. Checking for volume info only when audio data present. 
+        try:
+            data2 = conn2.recv(2)    #exceptions appear when data not available 
+        except socket.timeout as err:
+            print("timeout")
+        except socket.error as err:
+            print("error")
+        else:
+            volume = data2.decode('utf8')
+            set_master_volume(int(volume))
+        i = 0
 
 wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
 wf.setnchannels(CHANNELS)
@@ -48,3 +77,8 @@ stream.stop_stream()
 stream.close()
 p.terminate()
 conn.close()
+conn2.close()
+
+
+
+
